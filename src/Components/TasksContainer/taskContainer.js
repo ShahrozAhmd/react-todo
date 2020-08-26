8c6b35f1a6603d9ab58f18222069a85285ca2fa6
Shahroz Ahmed <shahrozahmed286@gmail.com>
2020-08-26 20:27:29 +0500

    added a container to display all the todo list

--- src/Components/TasksContainer/taskContainer.js
+++ src/Components/TasksContainer/taskContainer.js
@@ -1,10 +1,20 @@
-import React from 'react'
-import TaskItem from './TaskItem/taskItem'
+import React from "react";
+import classes from './taskContainer.module.css'
+import TaskItem from "./TaskItem/taskItem";
 
-export default function TaskContainer() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default function TaskContainer(props) {
+  return (
+    <div className = {classes.container}>
+      <div>
+        <table>
+          <tr className = {classes.Table}>
+            <th>Sno</th>
+            <th>Task</th>
+            <th>Date</th>
+            <th>Action</th>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
 }
