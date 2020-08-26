a6a537c4edb9bece84126081d513bd6f5f4cf1dc
Shahroz Ahmed <shahrozahmed286@gmail.com>
2020-08-27 00:09:42 +0500

    just setup complete tasklist container with demo data and design except the action buttons

--- src/Components/TasksContainer/TaskItem/taskItem.js
+++ src/Components/TasksContainer/TaskItem/taskItem.js
@@ -1,10 +1,16 @@
-import React from 'react'
-import ActionButton from '../../ActionButtons/actionButton'
+import React from "react";
+import classes from "./taskItem.module.css";
+import ActionButton from "../../ActionButtons/actionButton";
 
-export default function TaskItem() {
-    return (
-        <div>
-            
-        </div>
-    )
+export default function TaskItem(props) {
+  return (
+    <tr className={classes.Table}>
+      <td className={classes.Sno}>1.</td>
+      <td className={classes.Task}>
+        Do Breakfast in the morning preferably eggs and tea
+      </td>
+      <td className={classes.Date}>Aug, 26</td>
+      <td className={classes.Action}>Buttons</td>
+    </tr>
+  );
 }
--- src/Components/TasksContainer/taskContainer.js
+++ src/Components/TasksContainer/taskContainer.js
@@ -1,18 +1,22 @@
 import React from "react";
-import classes from './taskContainer.module.css'
+import classes from "./taskContainer.module.css";
 import TaskItem from "./TaskItem/taskItem";
 
 export default function TaskContainer(props) {
   return (
-    <div className = {classes.container}>
+    <div className={classes.container}>
       <div>
         <table>
-          <tr className = {classes.Table}>
-            <th>Sno</th>
-            <th>Task</th>
-            <th>Date</th>
-            <th>Action</th>
+          <tr className={classes.Table}>
+            <th className={classes.Sno}>Sno</th>
+            <th className={classes.Task}>Task</th>
+            <th className={classes.Date}>Date</th>
+            <th className={classes.Action}>Action</th>
           </tr>
+          {/* here I have to run .map to print rows as per data */}
+          <TaskItem />
+          <TaskItem />
+          <TaskItem />
         </table>
       </div>
     </div>
