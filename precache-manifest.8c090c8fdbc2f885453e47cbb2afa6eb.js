9b84e6b68fdbefdde8257a987e3410b40c6edb48
Shahroz Ahmed <shahrozahmed286@gmail.com>
2020-09-12 01:00:44 +0500

    Updates

---
+++ precache-manifest.8c090c8fdbc2f885453e47cbb2afa6eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba20c56b9687c2e987a492fe995c090e",
+    "url": "/todo.github.io/index.html"
+  },
+  {
+    "revision": "c30ba8319486b0e78f03",
+    "url": "/todo.github.io/static/css/main.52f240f2.chunk.css"
+  },
+  {
+    "revision": "30967fa11f5220aac8fd",
+    "url": "/todo.github.io/static/js/2.1bf70454.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo.github.io/static/js/2.1bf70454.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c30ba8319486b0e78f03",
+    "url": "/todo.github.io/static/js/main.ba172e6c.chunk.js"
+  },
+  {
+    "revision": "80323cbaa6a3a56af5c7",
+    "url": "/todo.github.io/static/js/runtime-main.b063e6fc.js"
+  }
+]);
