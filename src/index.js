87956287de8815965f955f39def1367101f8daf8
Shahroz Ahmed <shahrozahmed286@gmail.com>
2021-01-19 17:19:26 +0500

    Revert "redux config has been implemented and working perfectly fine"
    This reverts commit 490a12d55e75012cd4c968d78a42ebf82ef7b45d.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./Containers/App";
+import * as serviceWorker from "./serviceWorker";
 import { createStore } from "redux";
 import reducer from "./store/reducer";
 import { Provider } from "react-redux";
@@ -9,9 +10,15 @@ import { Provider } from "react-redux";
 const store = createStore(reducer);
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <React.StrictMode>
+    <Provider store = {store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
   document.getElementById("root")
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,24 +1,10 @@
-import * as actionTypes from "./action";
-
 const initialState = {
-  tasks: 0,
-};
+    tasks: [],
+}
 
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "ADD_TASK":
-      return { ...state, tasks: 1 };
-    case actionTypes.DELETE_TASK:
-      console.log("delte");
-      return { ...state };
-    case actionTypes.EDIT_TASK:
-      console.log("edit");
-      return { ...state };
-    case actionTypes.SAVE_EDITED_TASK:
-      console.log("save");
-      return { ...state };
-  }
-  return state;
-};
+const reducer  = (state = initialState, action) =>{
+    return state;
+}
 
 export default reducer;
+
